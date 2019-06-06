886c4436b95a9f14cd544ebe6a0e309adc436e24
Samyuktharallapalli <51381790+Samyuktharallapalli@users.noreply.github.com>
2019-06-06 10:37:02 -0400

    Update software.py

--- software.py
+++ software.py
@@ -1,7 +1,8 @@
 Python 3.6.7 (v3.6.7:6ec5cf24b7, Oct 20 2018, 13:35:33) [MSC v.1900 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> x=25
->>> y=35
+>>> y=45
+
 >>> print(x+y)
 60
 >>> 
