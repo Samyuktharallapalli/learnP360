3223aaf565f88ff05b0edd8f0bd4e418e1f4564e
Samyuktharallapalli <samyuktha.rallapalli@gmail.com>
2019-06-10 17:12:27 -0400

    added my first asssisgnment in python

---
+++ assignment1.py
@@ -0,0 +1,47 @@
+print("Hello, my name is rishabh. I love coding")
+print(" This is my 'first program'")
+
+value1=input("x ")
+value2=int(input("y "))
+value3=int(input("z "))
+
+print (value1, value2, value3)
+
+type(value1)
+
+
+print("hello!""my name is XYX")
+print("Hello I am a candidate")
+print("234.56")
+print("34")
+print("a+3j")
+
+
+x= 10+20*(45+67.0)
+print (x)
+type(x)
+
+x= (True and False) or (False)
+print(x)
+type(x)
+
+x= (True or True) and (not False and True)
+print(x)
+type(x)
+
+x= (3>89) or (34>32)
+print(x)
+type(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
