ef97291595528692848cd2495e211961f0e2ffd4
Samyuktharallapalli <51381790+Samyuktharallapalli@users.noreply.github.com>
2019-06-18 14:20:06 -0400

    assignments

---
+++ assignment1updated.py
@@ -0,0 +1,71 @@
+print('Hello, my name is "rishabh". I love coding')
+print(" This is my 'first program'")
+
+x= int(input())
+y=int(input())
+z=int(input())
+#not sure if it is correct
+print (x, y, z)
+
+type(x)
+
+
+print("hello! my name is XYX")
+print('"Hello I am a candidate"')
+print('"234.56"')
+print('"34"')
+print("a+3j")
+
+
+x= 10+20*(45+67.0)
+print (x)
+type(x)
+
+x=(True and False) or False# doesnt take the AND or OR
+print(x)
+type(x)
+
+x=(TRUE and TRUE) and (not False and True)
+print(x)
+type(x)
+
+x=(3>89) or (34>32)
+print(x)
+type(x)
+
+x=not True and False
+print(x)
+type(x)
+
+a=int(input("write a num"))
+(a%2==0 and a%5==0)
+if True:
+    print('"Hurryay it is what i am looking for"')	
+	
+else:
+
+    print('"wrong input"')	
+	
+	
+	
+j=range(10,50)
+l=int(input("type a num")	
+(l in j)
+if True:
+    print('"Yes i am in range"')
+else:
+	print ('"Oops"')
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assignment2.py.py
@@ -0,0 +1,45 @@
+# question 1
+x = [1, 2, 3, 4, [10, 20, 30, 40, [100, 200, 300, 400], "rishabh_", 5 + 5j], 4000]
+
+print(x[4][0], x[4][1], x[4][6], x[4][4][2], x[4][4][3])
+
+print(x[3], x[4][4], x[4][5])
+
+# question 2
+
+y = range(1, 22)
+print(y)
+
+for i in y:
+    i = (i + 2) + (i + 2)
+    i = (i + 1) + (i + 1)
+    print(i)
+	
+# Question 3
+
+
+# question4
+
+a = range(1, 51)
+print(a)
+for i in a:
+    i = (i ** 3)
+    print('i')
+	
+# question5
+
+# question6
+
+str = ["Hello world I am learning python"]
+print(str)
+
+# question 7
+
+mylist = int(input())
+
+if mylist == range(0,100):
+    print("true")
+else:
+    print("false")
+	
+
