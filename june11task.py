2c9ac74d757a118ad1bc6a426a898bcb1cd39cef
Samyuktharallapalli <51381790+Samyuktharallapalli@users.noreply.github.com>
2019-06-19 11:19:59 -0400

    june11 task

---
+++ june11task.py
@@ -0,0 +1,35 @@
+# question1
+
+n1=int(input("enter a number"))
+n2=int(input("enter a number"))
+n3=int(input("enter a number"))
+max1=max(n1, n2, n3)
+min1=min(n1, n2, n3)
+
+if max1==n1:
+    print("the maximum integer is",n1)
+elif max1==n2:
+    print("the maximum integer is",n2)
+elif max1==n3:
+    print("the maximum integer is",n3)
+elif min1==n1:
+    print("the minimum integer is",n1)
+elif min1==n2:
+    print("the minimum integer is",n2)
+elif min1==n3:
+    print("the minimum integer is",n3)
+
+
+#can we also do this in a different way??
+
+n1=int(input("enter a number"))
+n2=int(input("enter a number"))
+n3=int(input("enter a number"))
+max1=max(n1, n2, n3)
+min1=min(n1, n2, n3)
+
+print(max1)
+print(min1)
+
+	
+	
