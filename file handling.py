c0186fe5428c2723f51c6d429ec34597b9fdb568
Samyuktharallapalli <51381790+Samyuktharallapalli@users.noreply.github.com>
2019-06-25 22:49:35 -0400

    file handling

--- file handling.py
+++ file handling.py
@@ -1,8 +1,10 @@
 try:
     f = open(xfile.txt,'w')
     f.write("checking how file handling works")
+    
 except:
     print("look's like its not working")
+    
 finally:
     print("anyways, i am always exexcuted")
 
